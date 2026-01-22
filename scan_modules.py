5d8a2e0eb02e0047fb3c3d43c66fdf9b30b81c6f
monicagiraldochica <165931199+monicagiraldochica@users.noreply.github.com>
2026-01-22 10:47:32 -0600

    Update scan_modules.py
    create main()

--- scan_modules.py
+++ scan_modules.py
@@ -1,10 +1,17 @@
 import pandas as pd
 from pathlib import Path
 
+root_path = Path("/hpc/modulefiles")
+
 def scanModules() -> pd.DataFrame:
     rows = []
-    root_path = Path("/hpc/modulefiles")
 
     for lua in root_path.rglob("*.lua"):
         print(lua)
         break
+
+def main():
+    scanModules()
+
+if __name__ == "__main__":
+    main()
