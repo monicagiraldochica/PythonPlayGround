6f1a64d7db2a3893f2adaaeb5267dcdb2d260b99
monicagiraldochica <165931199+monicagiraldochica@users.noreply.github.com>
2026-01-22 11:56:23 -0600

    Update scan_modules.py

--- scan_modules.py
+++ scan_modules.py
@@ -1,5 +1,7 @@
 import pandas as pd
 from pathlib import Path
+from datetime import datetime
+import stat
 
 root_path = Path("/hpc/modulefiles")
 
@@ -9,13 +11,14 @@ def scanModules() -> pd.DataFrame:
     for lua in root_path.rglob("*/*.lua"):
         parts = lua.parts[1:]
         module = parts[2]
-        print(module)
         version = Path(parts[3]).stem.lstrip('.')
-        print(version)
         hidden = parts[3].startswith(".")
         print(hidden)
         st = lua.stat()
-        print(st)
+        readable_by_others = bool(st.st_mode & stat.S_IROTH)
+        print(readable_by_others)
+        print(datetime.fromtimestamp(st.st_mtime))
+        print(datetime.fromtimestamp(st.st_atime))
         break
 
 def main():
