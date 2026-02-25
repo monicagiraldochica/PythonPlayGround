a3caa21ceeed5cc21ed2d59087484bbdbcfb2b92
monicagiraldochica <165931199+monicagiraldochica@users.noreply.github.com>
2026-02-25 11:17:31 -0600

    Update InstallPerlPackages.py

--- InstallPerlPackages.py
+++ InstallPerlPackages.py
@@ -31,7 +31,7 @@ def check_module(mdl: str) -> int:
             return 0
         
         elif is_dispatcher:
-            cmd = f"perl -e use Log::Report (); require {mdl}; print \"Installed\""
+            cmd = f"perl -e 'use Log::Report (); require {mdl}; print \"Installed\"'"
             print(f"Running: {cmd}\n")
             result = subprocess.run(cmd, shell=True, capture_output=True, text=True, check=True)
             if result.returncode==0 and result.stdout.strip()=="Installed":
