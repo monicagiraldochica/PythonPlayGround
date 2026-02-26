057a3e2fe246af9d5e257a90f093cba61d0b2630
monicagiraldochica <165931199+monicagiraldochica@users.noreply.github.com>
2026-02-26 12:41:31 -0600

    Update installRpackages.py

--- installRpackages.py
+++ installRpackages.py
@@ -426,10 +426,10 @@ def main():
 				print(f"{pkg},{msg}\n")
 			if not success:
 				with open(file1, "a") as f1:
-					f1.write(f"{pkg},{msg.replace('\n',' ')}")
+					f1.write(f"{pkg},{msg}")
 			else:
 				with open(file2,'a') as f2:
-					f2.write(f"{pkg},{msg.replace('\n',' ')}")
+					f2.write(f"{pkg},{msg}")
 
 if __name__ == "__main__":
     main()
