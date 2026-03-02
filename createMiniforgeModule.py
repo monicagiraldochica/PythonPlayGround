2a32b14b990231a08f4e52fd0f886c83bc2b97dd
monicagiraldochica <165931199+monicagiraldochica@users.noreply.github.com>
2026-03-02 10:10:30 -0600

    Update createMiniforgeModule.py

--- createMiniforgeModule.py
+++ createMiniforgeModule.py
@@ -31,8 +31,8 @@ def availableModules(pkg):
     result = subprocess.run(["bash", "-lc", cmd], check=True, capture_output=True, text=True)
     out = (result.stdout or "") + (result.stderr or "")
     print(f"out: {out}")
-    match = re.search(rf'^\s*{re.escape(pkg)}/\d+(?:\.\d+)*\s*$', out, flags=re.MULTILINE)
-    print(f"match: {match}")
+    matches = re.findall(rf'^\s*{re.escape(pkg)}/\d+(?:\.\d+)*\s*$', out, flags=re.MULTILINE)
+    print(f"match: {matches}")
     #pat = re.compile(rf'^{re.escape(pkg)}/\d+(?:\.\d+)*\s*$', re.MULTILINE)
     #matches = pat.findall(out)
 
