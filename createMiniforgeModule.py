fc825ab1fa58add5d05ac27b0dac0253f6d692fc
monicagiraldochica <165931199+monicagiraldochica@users.noreply.github.com>
2026-03-02 09:42:31 -0600

    Create createMiniforgeModule.py

---
+++ createMiniforgeModule.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python3
+__author__ = "Monica Keith"
+
+import subprocess
+import sys
+import argparse
+import re
+
+def parse_arguments():
+    parser = argparse.ArgumentParser(description="Install bew nodule using miniforge")
+    parser.add_argument("--main-package", help="Name of the module to be created", required=True)
+    parser.add_argument("--version", help="Module version", required=True)
+    args = parser.parse_args()
+
+    return [args.main_package, args.version]
+
+def getCondaVersion():
+    try:
+        result = subprocess.run(["conda","--version"], check=True, capture_output=True, text=True).stdout
+        match = re.search(r"(\d+\.\d+\.\d+)", result)
+        return match.group(1) if match else None
+    
+    except FileNotFoundError:
+        return None
+    
+    except subprocess.CalledProcessError:
+        return None
+
+def availableModules(pkg):
+    result = subprocess.run(["ml", "avail", pkg], check=True, capture_output=True, text=True).stdout
+    pat = re.compile(rf'^{re.escape(pkg)}/\d+(?:\.\d+)*\s*$', re.MULTILINE)
+    matches = pat.findall(result)
+
+    return matches if matches else []
+
+def main():
+    # Check conda version
+    rVers = getCondaVersion()
+    if rVers is None:
+        print("Miniforge loaded. Run: module load miniforge")
+        sys.exit(1)
+
+    [main_package, version] = parse_arguments()
+
+    # Check if the module is already installed
+    print(availableModules("hicexplorer"))
+    print(availableModules("baqlava"))
+
+    if __name__ == "__main__":
+        main()
