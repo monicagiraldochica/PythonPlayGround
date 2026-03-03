4d02072c7d8f80ee48dd33dcc5c7ea4019cfb091
monicagiraldochica <165931199+monicagiraldochica@users.noreply.github.com>
2026-03-03 13:04:36 -0600

    edit createMiniforgeModule

--- createMiniforgeModule.py
+++ createMiniforgeModule.py
@@ -6,6 +6,8 @@ import sys
 import argparse
 import re
 import os
+import shutil
+import textwrap
 
 def parse_arguments():
     parser = argparse.ArgumentParser(description="Install bew nodule using miniforge")
@@ -68,12 +70,20 @@ def main():
 
     [main_package, version] = parse_arguments()
 
+    # Paths
+    build_path = f"/adminfs/builds/{main_package}/git_repos"
+    ml_folder = f"/hpc/modulefiles/{main_package}"
+    new_ml = f"{ml_folder}/{version}.lua"
+    forge_envs = "/hpc/apps/miniforge/envs"
+    forge_path = f"{forge_envs}/{env_name}/bin"
+    apps_path = f"/hpc/apps/{main_package}"
+    db_folder = f"/hpc/refdata/{main_package}"
+
     input("sudo su - [Enter]")
     input("ml load miniforge [Enter]")
 
     ml_avail = availableModules(main_package)
     create_env = True
-    ml_folder = f"/hpc/modulefiles/{main_package}"
 
     # The module is already installed
     if f"{main_package}/{version}" in ml_avail:
@@ -86,8 +96,7 @@ def main():
             sys.exit(1)
     
     else:
-        downloads = downloadedMiniforgeVersions(main_package, "/hpc/apps/miniforge/envs")
-        apps_path = f"/hpc/apps/{main_package}"
+        downloads = downloadedMiniforgeVersions(main_package, forge_envs)
 
         if len(downloads)>0:
             msg = f"\nA previous miniforge environment was created for {main_package} ({', '.join(downloads)}), "
@@ -105,7 +114,7 @@ def main():
                 sys.exit()
 
         elif os.path.isdir(apps_path):
-            msg = f"{main_package} was previously downloaded in /hpc/apps/{main_package}, outside miniforge, but no module was created.\nContent of {apps_path}:\n{contentFolder(apps_path)}\nDo you want to proceed? [y/N]: "
+            msg = f"{main_package} was previously downloaded in {apps_path}, outside miniforge, but no module was created.\nContent of {apps_path}:\n{contentFolder(apps_path)}\nDo you want to proceed? [y/N]: "
 
             if input(msg).strip().lower() not in ("yes", "y"):
                 sys.exit()
@@ -129,7 +138,6 @@ def main():
     if input("\nDo you need to clone any repos? [y/N]: ").strip().lower() in ("y", "yes"):
         repos = input("https git repos divided by comma: ").split(",")
         if len(repos)>0:
-            build_path = f"/adminfs/builds/{main_package}/git_repos"
             if not os.path.isdir(build_path):
                 os.makedirs(build_path)
 
@@ -159,8 +167,8 @@ def main():
 
     if use_pip:
         which_pip = input("\nrun 'which pip' and paste here the output: ")
-        if which_pip!=f"/hpc/apps/miniforge/envs/{env_name}/bin/pip":
-            input(f"*** DO NOT PROCEED UNTIL YOU THE RESULT OF which pip IS /hpc/apps/miniforge/envs/{env_name}/bin/pip *** [Enter]")
+        if which_pip!=f"{forge_path}/pip":
+            input(f"*** DO NOT PROCEED UNTIL YOU THE RESULT OF which pip IS {forge_path}/pip *** [Enter]")
 
         print("\nAfter each pip install run 'conda list | grep <program>' to check that it was indeed installed, and run any tests.\nDo not proceed with the next dependency until the previous one is installed and tested.\nRemember to add the version of each dependency if a specific version is needed!\n")
         pips = input("List of programs to install using pip divided by comma: ").split(",")
@@ -168,7 +176,6 @@ def main():
             input(f"pip install {pip_install} [Enter]")
             input(f"conda list | grep {pip_install} [Enter]")
             msg = f"Run a test command for {pip_install}."
-            forge_path = f"/hpc/apps/miniforge/envs/{main_package}-{version}/bin/"
             if os.path.isdir(forge_path):
                 msg+=f"\nThe list of commands for {main_package} can be found in {forge_path}"
             input(f"{msg} [Enter]")
@@ -184,12 +191,109 @@ def main():
                     input(f"conda list | grep {line} [Enter]")
 
     if input("\nDo you need to download any databases? [y/N]: ").strip().lower() in ("yes", "y"):
-        db_folder = f"/hpc/refdata/{main_package}"
         if not os.path.isdir(db_folder):
             os.mkdir(db_folder)
         input(f"Download any databases to {db_folder} [Enter]")
 
-    print(ml_avail)
+    # Copy module file from a previous version
+    if not os.path.isfile(new_ml):
+        for ml in ml_avail.reverse():
+            ml_path = f"{ml_folder}/{ml}.lua"
+            if os.path.isfile(ml_path):
+                shutil.copy(ml_path, new_ml)
+                print(f"\nCopied {ml_path} to {new_ml}")
+                break
+
+    # Create a new module file
+    else:
+        print(f"\nCreating {new_ml}:")
+
+        print("Module help can have new lines. Press Ctrl-D when done.\nModule help:\n")
+        ml_help = sys.stdin.read().strip()
+        if input(f"Does {main_package} has a GUI? [y/N]: ").strip().lower() in ("y", "yes"):
+            msg="Make sure you connect using -XY flag if planning to use the GUI.\nFor Mac users: make sure you have XQuartz installed."
+            if len(ml_help)>0:
+                ml_help+=f"\n\n{msg}"
+            else:
+                ml_help=msg
+        py_files = contentFolder(forge_path).split("\n")
+        py_files = [f for f in py_files if f.endswith(".py")]
+        if len(py_files)>0 and len(msg)>0:
+            ml_help+=f"\n\nRun '{py_files[0]} -h' instead of 'python {py_files[0]} -h'"
+
+        print("\nCategory ideas: Applications, Bioinformatics, biology, genomics, imaging, neuroimaging, chemistry, statistics, devel, math, fluid dynamics, data analytics, deep learning, machine learning, system, graphics")
+        categories = input("Categories: ").strip()
+        desc = input("Description: ").strip()
+        url = input("URL: ")
+        init_line = 'execute{cmd="source " .. conda_dir .. "/etc/profile.d/conda.sh; conda activate " .. myModuleName() .. "-" .. myModuleVersion() .. "; export -f " .. funcs, modeA={"load"}}'
+        python_line = 'family("python")'
+
+        if not os.path.isfile("unload_cmd.txt"):
+            print("*** lua file will be incomplete, the content that goes under -- Unload environments and clear conda from environment could not be loaded ***")
+            unload_line = ""
+        else:
+            with open("unload_cmd.txt", "r") as fin:
+                unload_line = fin.read()
+
+        content = textwrap.dedent(f"""\
+        help([[
+        {ml_help}
+        ]])
+
+        whatis("Name:     "..myModuleName())
+        whatis("Version:  "..myModuleVersion())
+        whatis("Category: {categories}")
+        whatis("Description: {desc}")
+        whatis("URL: {url}")
+
+        local conda_dir = "/hpc/apps/miniforge"
+        local funcs = "conda __conda_activate __conda_hashr __conda_reactivate __conda_exe"
+
+        -- Initialize conda and activate environment
+        {init_line}
+
+        -- Unload environments and clear conda from environment
+        {unload_line}
+
+        -- Prevent from being loaded with another system python or conda environment
+        {python_line}
+        """)
+
+        if input("Does any variables need to be set? [y/N]: ").strip().lower() in ("yes", "y"):
+            array = input("VAR:value divided by comma: ").split(",")
+            if len(array)>0:
+                content+="\n"
+            for var_info in array:
+                pair = var_info.split(":")
+                if len(pair)==2:
+                    content+=f'setenv("{pair[0]}", "{pair[1]}")'
+
+        mdl_deps = input("List dependencies divided by comma: ").strip().lower().split(",")
+        if len(mdl_deps)>0:
+            content+="\n"
+            for dep in mdl_deps:
+                content+=f'depends_on("{dep}")'
+
+            if any(dep.startswith("python/3") for dep in mdl_deps):
+                content+="\n--set_alias(\"python\", \"python3\")"
+
+        if not os.path.isdir(ml_folder):
+            os.mkdir(ml_folder)
+
+        with open(new_ml, "w") as f1:
+            f1.write(content)
+
+        if len(ml_avail)>0:
+            print(f"\nOther available versions for this module: {', '.join(ml_avail)}")
+
+            if input("Will this be the default and highest version? [Y/n]: ").strip().lower() in ("n", "no"):
+                default = input("Default version: ").strip()
+                default_lua = f"{ml_folder}/{default}.lua"
+                if os.path.isfile(default_lua):
+                    os.symlink(default_lua, f"{ml_folder}/default")
+                    print(f"Symlink created from {default_lua} to {ml_folder}/default")
+
+    input(f"vi {new_ml} [Enter]")
 
 if __name__ == "__main__":
     main()
