f6107b303051b7158e87895f59f4b78cf42949c9
monicagiraldochica <165931199+monicagiraldochica@users.noreply.github.com>
2026-05-06 15:07:22 -0500

    Update createMiniforgeModule.py

--- createMiniforgeModule.py
+++ createMiniforgeModule.py
@@ -200,7 +200,7 @@ def main():
         input(f"Download any databases to {db_folder} [Enter]")
 
     # Copy module file from a previous version
-    if not os.path.isfile(new_ml):
+    if (not os.path.isfile(new_ml)) and (ml_avail is not None):
         for ml in ml_avail.reverse():
             ml_path = f"{ml_folder}/{ml}.lua"
             if os.path.isfile(ml_path):
