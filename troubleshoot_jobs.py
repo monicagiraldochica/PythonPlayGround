c787b8c4ca157fdae429bcbe661a6fa826d07f6b
monicagiraldochica <165931199+monicagiraldochica@users.noreply.github.com>
2026-06-02 13:09:05 -0500

    Update troubleshoot_jobs.py

--- troubleshoot_jobs.py
+++ troubleshoot_jobs.py
@@ -191,7 +191,7 @@ def main():
     cols = df.columns.values.tolist()
     if len(cols)>1 and cols[1].startswith("OOD"):
         print(f"This job ran in OOD: {cols[1].replace("OOD_", "")}")
-        print(df.loc["WorkDir", cols[1]])
+        print(df.loc[cols[1], "WorkDir"])
 
 if __name__ == "__main__":
     main()
