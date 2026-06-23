c9a500899f8c63338ca4db572b645d35f16538a2
monicagiraldochica <165931199+monicagiraldochica@users.noreply.github.com>
2026-06-23 12:46:30 -0500

    Update troubleshoot_jobs.py

--- troubleshoot_jobs.py
+++ troubleshoot_jobs.py
@@ -107,6 +107,7 @@ def parseTime(t: str) -> int:
     print(f"{days}-->{time_part}")
     hours, minutes, seconds = time_part.split(":")
     print(f"{hours}-->{minutes}-->{seconds}")
+    print(int(hours))
     return int(days)*86400 + int(hours)*3600 + int(minutes)*60 + seconds
 
 def editRunTime(walltime: str, runtime: str) -> str:
