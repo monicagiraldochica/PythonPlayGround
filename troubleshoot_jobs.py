1ea9745a3b4f7bd8ff7de27dfcd38408cb167a73
Monica Keith <monicagiraldochica@gmail.com>
2026-07-30 10:22:19 -0500

    Fix commands that attempt to get queue position for a non OOD job

--- troubleshoot_jobs.py
+++ troubleshoot_jobs.py
@@ -385,14 +385,11 @@ def getQueuePos_notOOD(jobID: str, partition: str):
     try:
         p1 = subprocess.Popen(["sprio", "-p", partition, "--sort", "-y"], stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
         
-        p2 = subprocess.Popen(["awk", "{print NR-1 $0}"], stdin= p1.stdout, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
+        p2 = subprocess.Popen(["awk", "'$1=="+jobID+" {print NR-1}'"], stdin= p1.stdout, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
         p1.stdout.close()
 
-        p3 = subprocess.Popen(["less", "+g", "-p", jobID], stdin=p2.stdout, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
-        p2.stdout.close()
-
-        stdout, stderr = p3.communicate()
-        if p3.returncode!=0:
+        stdout, stderr = p2.communicate()
+        if p2.returncode!=0:
             return "", stderr
         return stdout, stderr
 
