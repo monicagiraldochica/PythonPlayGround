78e43e80f2a50fca559f556dacb1538da9e28da5
Monica Keith <monicagiraldochica@gmail.com>
2026-07-30 11:06:32 -0500

    Add function to run piped commands in bash

--- installib.py
+++ installib.py
@@ -24,13 +24,38 @@ def runBash(cmd: list, output_file: str=""):
         stdout = "" if output_file else result.stdout
         return result.returncode, result.stderr, stdout
     
-    except subprocess.CalledProcessError as e:
+    except Exception as e:
         err = (e.stderr or e.stdout or str(e)).strip()
         return e.returncode, err, ""
     
     finally:
         if file_handle:
             file_handle.close()
+
+def runPipedCommands(commands: list[list]):
+    try:
+        processes = []
+        prev_stdout = None
+
+        for cmd in commands:
+            p = subprocess.Popen(cmd, stdin=prev_stdout, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
+
+            if prev_stdout is not None:
+                prev_stdout.close()
+
+            prev_stdout = p.stdout
+            processes.append(p)
+
+            stdout, stderr = processes[-1].communicate()
+
+            for p in processes[:-1]:
+                p.wait()
+            
+            return processes[-1].returncode, stderr, stdout
+        
+    except Exception as e:
+         err = (e.stderr or e.stdout or str(e)).strip()
+         return e.returncode, err, ""
 
 def checkPythonVers(req_major: int=0, req_minor: int=0, req_micro: int=0, exact_vers: bool=False):
     python_info = sys.version_info
