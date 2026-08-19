56a42b37dc4c86872a387f9b4d9414630a440205
Monica Keith <monicagiraldochica@gmail.com>
2026-08-19 14:20:38 -0500

    Check memory and CPU efficiency also in running jobs

--- troubleshoot_jobs.py
+++ troubleshoot_jobs.py
@@ -1134,6 +1134,26 @@ def main():
 
         # If not, check the normal logs
         else:
+            print(f"Run: jobstats {jobID}")
+            CPUpct = input("CPU efficiency (CPU utilization per node): ").replace("%", "")
+            if not isinstance(CPUpct, (int, float)):
+                print("Not a valid value, can't calculate the number of CPU being used.")
+            else:
+                CPUpct = float(CPUpct)
+                AllocTRES = simple_df.loc[simple_df["Field"] == "AllocTRES", "Value"].iloc[0]
+                if not AllocTRES.startswith("cpu="):
+                    print(f"Can't calculate the number of CPU being used. AllocTRES: {AllocTRES}.")
+                AllocCPUS = int(AllocTRES.split(",")[0].replace("cpu=", ""))
+                CPUused = round(AllocCPUS*CPUpct*100)
+                if CPUpct<5:
+                    print(f"This job is single threaded. It is requesting {AllocCPUS} CPUs, but using {CPUused}. CPU efficiency is {CPUpct}%. Ask the user to request only one CPU.")
+                elif CPUpct<20:
+                    print(f"There's a high chance that the job is single threaded. The user is requesting {AllocCPUS} CPUs, but using {CPUused}. CPU efficiency is {CPUpct}%. Check the code to make sure it's multi-threaded.")
+                elif CPUpct<50:
+                    print(f"There's a high chance the job is multi threaded, but it's using less CPUs ({CPUused}) than those requested ({AllocCPUS}).")
+
+            input("From the output of jobstats you can also check memory efficiency (CPU memory usage per node) to see if the user is over requesting memory.")
+
             if (input("\nIs the job running on GPU nodes? [y/N]: ").strip().lower() in ["y", "yes"]) and (input("Did the user requested at least the same number of CPUs as GPUs? [Y/n]: ").strip().lower() in ["n", "no"]):
                 print("""That will cause errors. You must reserve at least the same number of CPUs than GPUs.
                     GPUs are used in tandem with a CPU. The CPU executes the main program with the GPU being used at times to carry out specific functions.
